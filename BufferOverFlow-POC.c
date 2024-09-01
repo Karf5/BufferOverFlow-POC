666fd0942234b8c8394292a0423e5b33b09c8754
Karf5 <151993346+Karf5@users.noreply.github.com>
2024-09-02 00:05:22 +0300

    Update BufferOverFlow-POC.c

--- BufferOverFlow-POC.c
+++ BufferOverFlow-POC.c
@@ -17,15 +17,22 @@ char username[] = "Karf";
 char user_name_input[10];                      // Vulnerability Is Here In The [10] Value 
 int K = 0;                 // K Value It's 0 After The Exploit The K Value Will Changed
 
+// Print the initial value of K
 printf(" K Value Is : 0 \n");
 
-printf("Enter The Value Of Exploit : ");      // Enter A Long Chain Of Numbers In Value Of Exploit
+// Prompt the user to enter a string
+printf("Enter The Value Of Exploit : "); // Enter A Long Chain Of Numbers In Value Of Exploit
+
+// Read user input into the buffer
 scanf("%s" , user_name_input);            
 
+// Print the value of K after input
 printf("Exploit Success / K Value Changed To : %d \n" , K);  // Simple Messege To Let You Know The Exploit Is Working And To Showing K Value
 
 int D;
-printf("Buffer OverFlow Vulnerability Is Working And Exploitable \n");  // We Use This Messega To Let The app.exe Stop The CMD Window , Without It The app.exe Will Start And Exit In The Same Time, Too Lazy I Know #_#
+printf("Buffer OverFlow Vulnerability Is Working And Exploitable \n"); // We Use This Messega To Let The app.exe Stop The CMD Window , Without It The app.exe Will Start And Exit In The Same Time, Too Lazy I Know #_#
+
+// Wait for user input to keep the console open
 scanf("%d" , D);
 
 }
