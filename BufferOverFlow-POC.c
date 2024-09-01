ebcca1582a52d71e1b897878e43d45059711af81
Karf5 <151993346+Karf5@users.noreply.github.com>
2024-09-02 00:07:57 +0300

    Update BufferOverFlow-POC.c

--- BufferOverFlow-POC.c
+++ BufferOverFlow-POC.c
@@ -29,10 +29,10 @@ scanf("%s" , user_name_input);
 // Print the value of K after input
 printf("Exploit Success / K Value Changed To : %d \n" , K);  // Simple Messege To Let You Know The Exploit Is Working And To Showing K Value
 
-int D;
-printf("Buffer OverFlow Vulnerability Is Working And Exploitable \n"); // We Use This Messega To Let The app.exe Stop The CMD Window , Without It The app.exe Will Start And Exit In The Same Time, Too Lazy I Know #_#
 
 // Wait for user input to keep the console open
+int D;
+printf("Buffer OverFlow Vulnerability Is Working And Exploitable \n");
 scanf("%d" , D);
 
 }
